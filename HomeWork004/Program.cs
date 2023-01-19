f280efe5e8b6bfdf7f3fb9f92d3fd1cc2ccd49e5
natasha8775735@gmail.com <natalliakhadakouskaya>
2023-01-19 20:34:58 +0300

    HomeWork004

---
+++ HomeWork004/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 
+
+Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все четные числа от 1 до N.
+
+*/
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+
+while (i < N)
+{
+    Console.Write("Четное число: ");
+    Console.WriteLine(i);
+    i = i + 2;
+}
